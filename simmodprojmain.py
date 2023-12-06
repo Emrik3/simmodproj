cd3ecc6654118a1fd0f72ba406679bfc33bb5750
Emrik Erikson <69987178+Emrik3@users.noreply.github.com>
2023-12-06 17:14:01 +0100

    Funkar för jorden

--- simmodprojmain.py
+++ simmodprojmain.py
@@ -20,65 +20,60 @@ class Planet():
         self.y = radius0 * np.sin(theta0)
         
         self.poslist = [np.array([self.x, self.y])]
-        self.v = [v0]
+        self.v = [self.v0]
 
         self.xlist = [self.x]
         self.ylist = [self.y]
 
 
-    def attraction(self, other):
-        other_x, other_y = other.x, other.y
-        distance_x = other_x - self.x
-        distance_y = other_y - self.y
-        distance = (distance_x ** 2 + distance_y ** 2) ** (-.5)
-
-
 
 class Simulator():
-    def __init__(self, sun, earth, N = 300, dt = 3600*24):
+    def __init__(self, sun, planets, N = 365, dt = 3600*24):
         self.N = N # This is weird, changing T and dt changes stuff crazy
         self.sun = sun
-        self.earth = earth
+        self.planets = planets
         self.dt = dt
         self.T = N * dt
 
     def RK3(self):
         for i in range(0,3):
-            hat = -fo.hat(self.earth.poslist[i], (0, 0))
-            k1 = fo.acc(self.sun.mass, G, self.earth.poslist[i], np.array([0, 0]), hat)
-            k1v = self.earth.v0
+            hat = -fo.hat(self.planet.poslist[i], (0, 0))
+            k1 = fo.acc(self.sun.mass, G, self.planet.poslist[i], np.array([0, 0]), hat)
+            k1v = self.planet.v[i]
 
-            k2 = fo.acc(self.sun.mass, G,np.add(self.earth.poslist[i], self.dt*k1v), np.array([0, 0]), hat)
-            k2v = np.add(self.earth.v0, k1*self.dt)
+            k2 = fo.acc(self.sun.mass, G,np.add(self.planet.poslist[i], self.dt*k1v), np.array([0, 0]), hat)
+            k2v = np.add(self.planet.v[i], k1*self.dt)
 
-            k3 = fo.acc(self.sun.mass, G, np.add(self.earth.poslist[i], self.dt*k1v / 4, self.dt*k2v / 4), np.array([0,0]), hat)
-            k3v = np.add(self.earth.v0, self.dt * k1 / 4, self.dt * k2 / 4)
+            k3 = fo.acc(self.sun.mass, G, np.add(self.planet.poslist[i], self.dt*k1v / 4, self.dt*k2v / 4), np.array([0,0]), hat)
+            k3v = np.add(self.planet.v[i], self.dt * k1 / 4, self.dt * k2 / 4)
 
-            hold = [np.add(self.earth.v0, self.dt / 6 * (k1 + k2 + 4*k3)), np.add(self.earth.poslist[i], self.dt / 6 * (k1v + k2v + 4*k3v))]
-            self.earth.v.append(hold[0])
+            hold = [np.add(self.planet.v[i], self.dt / 6 * (k1 + k2 + 4*k3)), np.add(self.planet.poslist[i], self.dt / 6 * (k1v + k2v + 4*k3v))]
+            self.planet.v.append(hold[0])
         
-            self.earth.poslist.append(hold[1])
+            self.planet.poslist.append(hold[1])
 
     def AB4(self):
-        initpos = self.earth.poslist[0]
-        #self.earth.poslist = np.zeros(self.N)
-        #self.earth.poslist[0] = (initpos[0], initpos[1])
+        initpos = self.planet.poslist[0]
+        #self.planet.poslist = np.zeros(self.N)
+        #self.planet.poslist[0] = (initpos[0], initpos[1])
         a = []
-        self.RK3()
+        for planet in self.planets:
+            self.RK3(planet)
 
-        for i in range(0, 3):
-            hat = -fo.hat(self.earth.poslist[i], (0, 0))
-            a.append(fo.acc(self.sun.mass, G, self.earth.poslist[i], np.array([0,0]), hat))
+            for i in range(0, 3):
+                hat = -fo.hat(self.planet.poslist[i], (0, 0))
+                a.append(fo.acc(self.sun.mass, G, self.planet.poslist[i], np.array([0,0]), hat))
 
         for i in range(3, self.N):
-            hat = -fo.hat(self.earth.poslist[i], (0, 0))
-            a.append(fo.acc(self.sun.mass, G, self.earth.poslist[i], np.array([0, 0]), hat))
-            # Maybe make v0 an arrey so that AB4 on the velocity as well
-            hold = [np.add(self.earth.poslist[i], self.dt / 24 * (55 * self.earth.v[i] -59 * self.earth.v[i-1] + 37 * self.earth.v[i-2] - 9 * self.earth.v[i-3])), np.add(self.earth.v0, self.dt / 24 * (55*a[i] - 59*a[i-1] + 37*a[i-2] - 9*a[i-3]))]
-            
-            self.earth.v.append(hold[1]) # TODO: V IS WAY TO LARGE I THINK
-            print(la.norm(a[i]))
-            self.earth.poslist.append(hold[0])
+            for planet in self.planets:
+                hat = -fo.hat(self.planet.poslist[i], (0, 0))
+                a.append(fo.acc(self.sun.mass, G, self.planet.poslist[i], np.array([0, 0]), hat))
+                # Maybe make v0 an arrey so that AB4 on the velocity as well
+                hold = [np.add(self.planet.poslist[i], self.dt * (55 * self.planet.v[i] - 59 * self.planet.v[i-1] + 37 * self.planet.v[i-2] - 9 * self.planet.v[i-3]) / 24), np.add(self.planet.v[i], self.dt * (55*a[i] - 59*a[i-1] + 37*a[i-2] - 9*a[i-3]) / 24)]
+                self.planet.v.append(hold[1]) # TODO: V IS WAY TO LARGE I THINK
+                
+                self.planet.poslist.append(hold[0])
+
             
             
 
@@ -86,7 +81,10 @@ class Simulator():
 def main():
     sun = Planet(1.98892 * 10**30, 0, 0, 0)
     earth = Planet(5.972 * 10 ** 24, 149.6e6 * 1000, 29783, 0)
-    Simulator(sun, earth).AB4()
+    planets = [earth]
+
+    Simulator(sun, planets, N=365).AB4()
+    
     plt.plot((0, 0), '*y')
     plt.scatter(*zip(*earth.poslist))
     plt.legend(['Sun', 'Earths orbit'])
